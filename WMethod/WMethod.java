bb6749e3d2edfd9564c831764e4f9357a597a157
jrajewski <jrajew1@students.towson.edu>
2017-04-21 12:19:41 -0400

    Task1
    Added output for Task1 and the code change in WMethod.

--- WMethod/WMethod.java
+++ WMethod/WMethod.java
@@ -380,6 +380,9 @@ public class WMethod{
      pTableManager w = new pTableManager(FSM, numberOfStates, realInput);
      Vector <String> tests=generateTests(transitionCover, w); // Generate tests.
      Utilities.printAllTestCases(tests); // Print tests.
+
+     for(int i = 0; i < tests.size(); i++)
+    	 Utilities.runFSM(FSM, 1, tests.get(i).replace("", " "), " ");
      
      // TODO: 	Write the necessary code to iterate through all test cases and run them against
      // 		the FSM using the Utilities.runFSM() method. 
