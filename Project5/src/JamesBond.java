42cf774a9f967546854d7d68f4817bfe6a6bde7b
jrajewski <jrajew1@students.towson.edu>
2017-04-21 20:16:55 -0400

    Task 3
    I modified the code to generate the test cases for JamesBond. I changed
    the runFSM method to type boolean, that checks for if "007" is in the
    outputPattern variable. My diagram was constructed with Dia, a .png is
    also saved. I did not successfully implement JamesBond using regular
    expressions, only contains.

---
+++ Project5/src/JamesBond.java
@@ -0,0 +1,16 @@
+
+public class JamesBond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static boolean bondRegex(String input){
+		if(input.contains("007"))
+			return true;
+		else
+			return false;
+	}
+
+}
